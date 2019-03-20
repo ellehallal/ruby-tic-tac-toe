09d37332ac745a2f77556ce5ae27cad93e1fea4f
Elle Hallal <daniellehallal@gmail.com>
2019-03-20 11:34:07 +0000

    replace before each block with describe block

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,13 @@
 require 'game'
 
 RSpec.describe Game do
-  before :each do
-    @game = Game.new
-  end
+  describe 'prompting user' do
+    game = Game.new
 
-  it 'prompts the user for their move and returns the move as an integer' do
-    allow($stdin).to receive(:gets).and_return('1')
-    move = @game.prompt_make_move
-    expect(move).to eq(1)
+    it 'prompts the user for their move and returns the move as an integer' do
+      allow($stdin).to receive(:gets).and_return('1')
+      move = game.prompt_make_move
+      expect(move).to eq(1)
+    end
   end
 end
