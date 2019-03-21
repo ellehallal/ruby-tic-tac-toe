6b81a4494cacee8f5284c64a893a98c94e5c5052
Elle H <itsellej@gmail.com>
2019-03-21 09:21:39 +0000

    Add play_move() to Controller. Toggles current player after each move

--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -5,12 +5,24 @@ require 'game'
 require 'player'
 
 RSpec.describe Controller do
-  describe 'Controller: ' do
+  describe 'Controller: alternates current player after each move ' do
     board = Board.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
     display = Display.new
     player1 = Player.new('x')
     player2 = Player.new('o')
     game = Game.new(board, display, player1, player2)
     controller = Controller.new(game)
+
+    it 'player1 makes a move then current player toggles to player2' do
+      allow($stdin).to receive(:gets).and_return('1')
+      controller.play_move
+      expect(game.current_player.mark).to eq('o')
+    end
+
+    it 'player2 makes a move then current player toggles to player1' do
+      allow($stdin).to receive(:gets).and_return('9')
+      controller.play_move
+      expect(game.current_player.mark).to eq('x')
+    end
   end
 end
