bf52c06d116bcb415efa19e99fbdcbc2ed276d64
Elle Hallal <daniellehallal@gmail.com>
2019-03-25 08:33:23 +0000

    Add lib/ play.rb to start game

---
+++ lib/play.rb
@@ -0,0 +1,12 @@
+require_relative './game.rb'
+require_relative './board.rb'
+require_relative './player.rb'
+require_relative './display.rb'
+
+display = Display.new
+player1 = Player.new('x')
+player2 = Player.new('o')
+board = Board.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
+game = Game.new(board, display, player1, player2)
+
+game.play_move
