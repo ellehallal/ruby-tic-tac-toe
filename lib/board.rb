3d7989ea245c6ccd5afc8af755dc99f78f7546d0
Elle Hallal <daniellehallal@gmail.com>
2019-03-25 08:45:28 +0000

    board tracks remaining moves available

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,13 @@
 class Board
-  attr_reader :board
+  attr_reader :board, :attr_reader
 
   def initialize(board)
     @board = board
+    @moves_remaining = 9
   end
 
   def player_make_move(player_mark, position)
     @board[position - 1] = player_mark
+    @moves_remaining -= 1
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Board do
       expect(board.board).to eq([1, 2, 3, 'x', 5, 6, 7, 8, 9])
     end
   end
+
+  describe 'tracking remaining moves' do
+    it 'when a move is made, the number of moves remaining decrements by 1' do
+      board = Board.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
+      board.player_make_move('x', 4)
+      expect(board.moves_remaining).to eq(8)
+    end
+  end
 end
