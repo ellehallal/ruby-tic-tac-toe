61605272d7e4d986eb6f6b5d49e8177a10546f0e
Elle Hallal <daniellehallal@gmail.com>
2019-04-09 10:55:43 +0100

    Game Manager: refactor game_selection

--- lib/game_manager.rb
+++ lib/game_manager.rb
@@ -15,22 +15,35 @@ class GameManager
     choice == 'Y'
   end
 
+  def game_selection
+    selection = prompt_game_selection
+    validate_game_selection(selection)
+    selection
+  end
+
+  private
+
   def game_loop
     play_game = true
-    game_selection
     while play_game
+      game_selection
       @controller.main_game
       play_game = play_again?
       @game.reset_game
     end
   end
 
-  def game_selection
-    selection = 0
-    until selection.between?(1, 4)
-      @display.show_game_options
-      selection = $stdin.gets.to_i
-    end
-    selection
+  def prompt_game_selection
+    @display.show_game_options
+    selection = $stdin.gets.to_i
   end
+
+  def validate_game_selection(selection)
+    until selection.between?(1, 4)
+      @display.show_invalid_option_message
+      selection = prompt_game_selection
+    end
+  end
+
+  
 end
