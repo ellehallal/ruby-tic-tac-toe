367337b761cdff50ef2bed054d72587260d77d5a
Elle Hallal <daniellehallal@gmail.com>
2019-04-10 09:14:41 +0100

    Players: amend HumanPlayer & Computer player to initialize with @name. In CP, amend sleep to 1.5 and format output

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -1,9 +1,16 @@
 require_relative './player.rb'
 
 class ComputerPlayer < Player
+  attr_reader :name
+
+  def initialize(mark)
+    super(mark)
+    @name = 'Computer'
+  end
+
   def choose_move(available_moves)
-    puts "\nComputer is thinking..."
-    sleep(3)
+    puts "\n\nComputer is thinking. Please wait...\n\n"
+    sleep(1.5)
     computer_move = available_moves.sample
     puts "Computer has selected position #{computer_move}"
     computer_move
--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,7 +1,14 @@
 require_relative './player.rb'
 
 class HumanPlayer < Player
-  def choose_move(moves = 0)
+  attr_reader :name
+
+  def initialize(mark)
+    super(mark)
+    @name = 'Human'
+  end
+
+  def choose_move(*)
     $stdin.gets.chomp.to_i
   end
 end
