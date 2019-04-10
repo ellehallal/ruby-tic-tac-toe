7c4fc9686538091ce10b3c6fbd2aeef46e32699d
Elle Hallal <daniellehallal@gmail.com>
2019-04-10 11:05:46 +0100

    Controller - add @default_squares, amend main_game and play_move. Add player_selection, prompt_player1_selection, prompt_player2_selection, validate_game_selection. Amend tests to reflect setup changes

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,20 +1,65 @@
+require_relative './game_factory.rb'
+
 class Controller
   attr_reader :game
-  def initialize(game, display)
+  def initialize(display)
     @game = game
     @display = display
+    @default_squares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
-  def main_game
+  def create_game(player1, player2, squares = @default_squares)
+    @game = GameFactory.build(squares, player1, player2)
+  end
+
+  def main_game(squares = @default_squares)
+    players = player_selection
+    create_game(players[0], players[1], squares)
+
     play_move until @game.over?
     end_of_game
+    @game.reset_game
+  end
+
+  def player_selection
+    players = []
+
+    selection1 = prompt_player1_selection
+    validate_player_selection(selection1, 'player1')
+
+    selection2 = prompt_player2_selection
+    validate_player_selection(selection2, 'player2')
+
+    players.push(selection1, selection2)
   end
 
   private
 
+  def prompt_player1_selection
+    @display.ask_for_player1
+    $stdin.gets.chomp.downcase
+  end
+
+  def prompt_player2_selection
+    @display.ask_for_player2
+    $stdin.gets.chomp.downcase
+  end
+
+  def validate_player_selection(selection, player)
+    until %w[h c].include?(selection)
+      @display.show_invalid_option_message
+
+      if player == 'player1'
+        selection = prompt_player1_selection
+      elsif player == 'player2'
+        selection = prompt_player2_selection
+      end
+    end
+  end
+
   def play_move
     @display.display_board(@game.board.squares)
-    @display.show_current_player(@game.current_player_mark)
+    @display.show_current_player(@game.player_info[:mark], @game.player_info[:name])
     @display.ask_for_move
     @game.play_move(@display.invalid_move_message)
     @game.toggle_current_player
--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -4,37 +4,73 @@ require 'display'
 require 'game'
 require 'human_player'
 
-def controller_setup(squares)
-  board = Board.new(squares)
+def controller_setup
   display = Display.new
-  player1 = HumanPlayer.new('x')
-  player2 = HumanPlayer.new('o')
-  game = Game.new(board, player1, player2)
-  controller = Controller.new(game, display)
+  controller = Controller.new(display)
   controller
 end
 
 RSpec.describe Controller do
+  describe 'Game selection: ' do
+    it "returns user's selection of players as an array in lowercase" do
+      allow($stdin).to receive(:gets).and_return('C', 'H')
+      controller = controller_setup
+
+      selection = controller.player_selection
+
+      expect(selection).to eq(%w[c h])
+    end
+
+    it 'prompts user for input again if input is not h or c' do
+      allow($stdin).to receive(:gets).and_return('X', 'C', 'H')
+      $stdout = StringIO.new
+      controller = controller_setup
+
+      controller.player_selection
+      output = $stdout.string.split("\n")
+
+      expect(output[0])
+        .to eq('Please select player 1 (h = human, c = computer):')
+      expect(output[1])
+        .to eq('Invalid option selected. Please try again:')
+      expect(output[2])
+        .to eq('Please select player 1 (h = human, c = computer):')
+      expect(output[3])
+        .to eq('Please select player 2 (h = human, c = computer):')
+    end
+  end
+
+  describe 'Creating a Game: ' do
+    it 'sets @game to a new instance of Game' do
+      controller = controller_setup
+
+      controller.create_game('h', 'c')
+
+      expect(controller.game).to be_an_instance_of(Game)
+    end
+  end
+
   describe 'Playing the game: ' do
     it 'plays a game that ends with a tie' do
-      controller = controller_setup(%w[x o x o o x x x o])
+      allow($stdin).to receive(:gets).and_return('h', 'c')
       $stdout = StringIO.new
+      controller = controller_setup
 
-      controller.main_game
-      outputs = $stdout.string.split("\n")
+      controller.main_game(%w[x o x o o x x x o])
+      output = $stdout.string.split("\n")
 
-      expect(outputs.last).to eq('The game is a tie!')
+      expect(output.last).to eq('The game is a tie!')
     end
 
     it 'plays a game that ends with a winning player' do
-      controller = controller_setup([1, 'x', 'x', 4, 'o', 'x', 'x', 8, 'o'])
-      allow($stdin).to receive(:gets).and_return('8', '4', '1')
+      allow($stdin).to receive(:gets).and_return('h', 'h', '8', '4', '1')
       $stdout = StringIO.new
+      controller = controller_setup
 
-      controller.main_game
-      outputs = $stdout.string.split("\n")
+      controller.main_game([1, 'x', 'x', 4, 'o', 'x', 'x', 8, 'o'])
+      output = $stdout.string.split("\n")
 
-      expect(outputs.last).to eq('x is the winner!')
+      expect(output.last).to eq('x is the winner!')
     end
   end
 end
