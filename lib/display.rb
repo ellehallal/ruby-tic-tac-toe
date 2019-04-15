4adaf2acc994815d162304eb7d3895f81b64021b
Elle H <itsellej@gmail.com>
2019-04-15 19:24:02 +0100

    Add DisplayColour to Display. Amend Display tests to check for printed output, rather than colour

--- lib/display.rb
+++ lib/display.rb
@@ -1,24 +1,61 @@
+require_relative 'display_colour'
+
 class Display
+  def initialize(display_colour)
+    @display_colour = display_colour
+  end
+
+  def show_welcome_message
+    print "Let's play Tic Tac Toe!\n"
+  end
+
   def display_board(board)
     clear_screen
-    print ''"
+
+    output = ''"
      #{board[0]} | #{board[1]} | #{board[2]}
     -----------
      #{board[3]} | #{board[4]} | #{board[5]}
     -----------
-     #{board[6]} | #{board[7]} | #{board[8]}\n"''
+     #{board[6]} | #{board[7]} | #{board[8]}\n\n"''
+
+    print @display_colour.board_colour(output)
+  end
+
+  def ask_for_player_selection(player_number)
+    output = "Please select player #{player_number} (h = human, c = computer):\n"
+    print @display_colour.input_prompt_colour(output)
   end
 
   def ask_for_move
-    print "Choose a position from 1-9: \n"
+    output = "Choose a position from 1-9: \n"
+
+    print @display_colour.input_prompt_colour(output)
+  end
+
+  def show_invalid_option_message
+    output = "Invalid option selected. Please try again:\n"
+    print @display_colour.warning_colour(output)
   end
 
   def show_current_player(mark, name)
-    print "#{mark} (#{name}), play a move:\n"
+    output = "#{mark} (#{name}), play a move:\n"
+
+    print @display_colour.player_colour(mark, output)
   end
 
   def invalid_move_message
-    print "You have entered an invalid move. Please try again:\n"
+    output = "You have entered an invalid move. Please try again:\n"
+
+    print @display_colour.warning_colour(output)
+  end
+
+  def show_computer_thinking(mark)
+    print "\nComputer (#{mark}) is thinking. Please wait...\n"
+  end
+
+  def show_computer_move(move, mark)
+    print "Computer (#{mark}) has selected position #{move}\n"
   end
 
   def show_game_outcome(outcome)
@@ -30,45 +67,25 @@ class Display
   end
 
   def ask_play_again
-    print "Play again? (Y/N):\n"
+    output = "Play again? (Y/N):\n"
+
+    print @display_colour.input_prompt_colour(output)
   end
 
   def show_exit_message
     print "Thanks for playing Tic Tac Toe!\n"
   end
 
-  def show_welcome_message
-    print "Let's play Tic Tac Toe!\n"
-  end
-
-  def show_invalid_option_message
-    print "Invalid option selected. Please try again:\n"
-  end
-
-  def ask_for_player_selection(player_number)
-    print "Please select player #{player_number} (h = human, c = computer):\n"
-  end
-
-  def ask_for_player2
-    print "Please select player 2 (h = human, c = computer):\n"
-  end
-
-  def show_computer_thinking
-    print "\nComputer is thinking. Please wait...\n"
-  end
-
-  def show_computer_move(move)
-    print "Computer has selected position #{move}\n"
-  end
-
   private
 
-  def show_winner_message(player_mark)
-    print "#{player_mark} is the winner!\n"
+  def show_winner_message(mark)
+    output = "#{mark} is the winner!\n"
+    print @display_colour.player_colour(mark, output)
   end
 
   def show_tie_message
-    print "The game is a tie!\n"
+    output = "The game is a tie!\n"
+    print @display_colour.tie_outcome_colour(output)
   end
 
   private
--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -1,134 +1,170 @@
 require 'display'
+require 'display_colour'
 require 'board'
 
 def display_setup
-  Display.new
+  display_colour = DisplayColour.new
+  Display.new(display_colour)
 end
 
 RSpec.describe Display do
   context 'Displaying a grid:' do
     display = display_setup
+    $stdout = StringIO.new
 
     it 'accepts a grid as an argument and displays it' do
-      expect { display.display_board([1, 2, 3, 4, 5, 6, 7, 8, 9]) }
-        .to output(''"
-     1 | 2 | 3
-    -----------
-     4 | 5 | 6
-    -----------
-     7 | 8 | 9\n"'').to_stdout
+      display.display_board([1, 2, 3, 4, 5, 6, 7, 8, 9])
+      output = $stdout.string
+
+      expect(output).to include('1 | 2 | 3')
+      expect(output).to include('4 | 5 | 6')
+      expect(output).to include('7 | 8 | 9')
     end
   end
 
   context 'Asking the user for a move:' do
     display = display_setup
-    board = Board.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    $stdout = StringIO.new
 
     it 'displays "Choose a position from 1-9"' do
       allow($stdin).to receive(:gets).and_return('1')
 
-      expect { display.ask_for_move }
-        .to output("Choose a position from 1-9: \n").to_stdout
+      display.ask_for_move
+      output = $stdout.string
+
+      expect(output).to include('Choose a position from 1-9:')
     end
   end
 
   context 'Displaying the current player:' do
     display = display_setup
+    $stdout = StringIO.new
 
     it 'displays "The current player is x"' do
-      expect { display.show_current_player('x', 'Computer') }
-        .to output("x (Computer), play a move:\n").to_stdout
+      display.show_current_player('x', 'Computer')
+      output = $stdout.string
+
+      expect(output).to include('x (Computer), play a move:')
     end
   end
 
   context 'Displays a message when a user enters an incorrect move:' do
     display = display_setup
+    $stdout = StringIO.new
 
-    it 'returns "You have entered an invalid move. Please try again:"' do
-      expect { display.invalid_move_message }
-        .to output("You have entered an invalid move. Please try again:\n").to_stdout
+    it 'Displays "You have entered an invalid move. Please try again:"' do
+      display.invalid_move_message
+      output = $stdout.string
+
+      expect(output)
+        .to include('You have entered an invalid move. Please try again:')
     end
   end
 
   context 'Displays message, depending on game outcome' do
     display = display_setup
+    $stdout = StringIO.new
 
     it "displays 'x is the winner!' when the outcome is x" do
       outcome = 'x'
 
-      expect { display.show_game_outcome(outcome) }
-        .to output("x is the winner!\n").to_stdout
+      display.show_game_outcome(outcome)
+      output = $stdout.string
+
+      expect(output).to include('x is the winner!')
     end
 
     it "displays 'The game is a tie!\n' when the outcome is a tie" do
       outcome = 'tie'
 
-      expect { display.show_game_outcome(outcome) }
-        .to output("The game is a tie!\n").to_stdout
+      display.show_game_outcome(outcome)
+      output = $stdout.string
+
+      expect(output).to include('The game is a tie!')
     end
   end
 
   context 'Displays messages after game has ended ' do
     display = display_setup
+    $stdout = StringIO.new
 
     it 'displays "Play again? (Y/N):"' do
       allow($stdin).to receive(:gets).and_return('Y')
 
-      expect { display.ask_play_again }
-        .to output("Play again? (Y/N):\n").to_stdout
+      display.ask_play_again
+      output = $stdout.string
+
+      expect(output).to include('Play again? (Y/N):')
     end
 
     it 'displays "Thanks for playing Tic Tac Toe!"' do
-      expect { display.show_exit_message }
-        .to output("Thanks for playing Tic Tac Toe!\n").to_stdout
+      display.show_exit_message
+      output = $stdout.string
+
+      expect(output).to include('Thanks for playing Tic Tac Toe!')
     end
   end
 
   context 'Game options ' do
     display = display_setup
+    $stdout = StringIO.new
 
     it 'displays the available game options a user can select' do
-      display = display_setup
+      display.show_welcome_message
+      output = $stdout.string
 
-      expect { display.show_welcome_message }
-        .to output("Let's play Tic Tac Toe!\n").to_stdout
+      expect(output)
+        .to include("Let's play Tic Tac Toe!")
     end
 
     it 'displays "Invalid game option selected. Please try again:"' do
-      expect { display.show_invalid_option_message }
-        .to output("Invalid option selected. Please try again:\n").to_stdout
+      display.show_invalid_option_message
+      output = $stdout.string
+
+      expect(output)
+        .to include('Invalid option selected. Please try again:')
     end
   end
 
   context 'Player types ' do
     display = display_setup
+    $stdout = StringIO.new
 
     it "displays 'Please select player 1 (h = human, c = computer):'" do
-      display = display_setup
+      display.ask_for_player_selection(1)
+      output = $stdout.string
 
-      expect { display.ask_for_player_selection(2) }
-        .to output("Please select player 2 (h = human, c = computer):\n").to_stdout
+      expect(output)
+        .to include('Please select player 1 (h = human, c = computer):')
     end
 
     it "displays 'Please select player 2 (h = human, c = computer):'" do
-      expect { display.ask_for_player2 }
-        .to output("Please select player 2 (h = human, c = computer):\n").to_stdout
+      display.ask_for_player_selection(2)
+      output = $stdout.string
+
+      expect(output)
+        .to include('Please select player 2 (h = human, c = computer):')
     end
   end
 
   context 'Computer move ' do
     display = display_setup
+    $stdout = StringIO.new
 
-    it "displays 'Computer is thinking. Please wait...'" do
-      display = display_setup
+    it "displays 'Computer (x) is thinking. Please wait...'" do
+      display.show_computer_thinking('x')
+      output = $stdout.string
 
-      expect { display.show_computer_thinking }
-        .to output("\nComputer is thinking. Please wait...\n").to_stdout
+      expect(output)
+        .to include('Computer (x) is thinking. Please wait...')
     end
 
-    it 'displays "Computer has selected position 3"' do
-      expect { display.show_computer_move(3) }
-        .to output("Computer has selected position 3\n").to_stdout
+    it 'displays "Computer (o) has selected position 3"' do
+      display.show_computer_move(3, 'o')
+      output = $stdout.string
+
+      expect(output)
+        .to include('Computer (o) has selected position 3')
     end
   end
 end
