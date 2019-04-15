2728cacb4de11de05d05a48a17c83a37714d9781
Elle H <itsellej@gmail.com>
2019-04-15 18:52:32 +0100

    Install paint gem. Create DisplayColour class, with methods for displaying output in colour

---
+++ lib/display_colour.rb
@@ -0,0 +1,27 @@
+require 'paint'
+
+class DisplayColour
+  def input_prompt_colour(output)
+    Paint[output, :bright, '#40F99B']
+  end
+
+  def warning_colour(output)
+    Paint[output, :inverse, :bold, '#DE3C4B']
+  end
+
+  def board_colour(output)
+    Paint[output, :bright, '#00FFDD']
+  end
+
+  def player_colour(mark, output)
+    if mark == 'x'
+      Paint[output, :inverse, :bright, '#FFDC00']
+    else
+      Paint[output, :inverse, :bright, '#FF6D99']
+    end
+  end
+
+  def tie_outcome_colour(output)
+    Paint[output, :inverse, :bright, '#E2E3FF']
+  end
+end
