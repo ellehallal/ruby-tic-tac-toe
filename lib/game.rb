02bfdf57711b597478e18096ea576bc0eb2b0299
Elle Hallal <daniellehallal@gmail.com>
2019-04-23 16:14:56 +0100

    Game: add @opponent. Amend methods to add @opponent

--- lib/game.rb
+++ lib/game.rb
@@ -1,25 +1,26 @@
 class Game
-  attr_reader :board, :current_player
+  attr_reader :board, :current_player, :opponent
 
   def initialize(board, player1, player2)
     @board = board
     @player1 = player1
     @player2 = player2
     @current_player = @player1
+    @opponent = @player2
   end
 
   def play_move
-    player_move = @current_player.choose_move(board, @player1.mark, @player2.mark)
+    player_move = @current_player.choose_move(board, @opponent.mark)
     board.player_make_move(@current_player.mark, player_move)
     toggle_current_player
   end
 
   def over?
-    board.complete? || board.winning_player_exists?(@player1.mark, @player2.mark)
+    board.complete? || board.winning_player_exists?(@current_player.mark, @opponent.mark)
   end
 
   def tie_or_won
-    if board.winning_player_exists?(@player1.mark, @player2.mark)
+    if board.winning_player_exists?(@current_player.mark, @opponent.mark)
       winning_player
     else
       'tie'
@@ -29,18 +30,20 @@ class Game
   private
 
   def winning_player
-    if board.winning_line?(@player1.mark)
-      @player1.mark
+    if board.winning_line?(@current_player.mark)
+      @current_player.mark
     else
-      @player2.mark
+      @opponent.mark
     end
   end
 
   def toggle_current_player
     if @current_player == @player1
       @current_player = @player2
+      @opponent = @player1
     else
       @current_player = @player1
+      @opponent = @player2
     end
   end
 end
