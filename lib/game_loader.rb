36e0030860d0e0dc50d3540294bbe2fdf631313a
Elle Hallal <daniellehallal@gmail.com>
2019-04-25 14:06:04 +0100

    GameLoader: add exists? to check if key (game name) exists. add load_file to load file contents and return empty hash if contents are empty

--- lib/game_loader.rb
+++ lib/game_loader.rb
@@ -1,7 +1,19 @@
 class GameLoader
 
   def load(filename, name)
-    file = YAML.load_file(filename)
+    file = load_file(filename)
     file[name]
+  end
+
+  def exists?(filename, name)
+    file = load_file(filename)
+    file.key?(name)
+  end
+
+  private
+
+  def load_file(filename)
+    file = YAML.load_file(filename)
+    file == false ? {} : file
   end
 end
