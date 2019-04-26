5fc56bae2bd4a806660557f1429efb19dc650dd3
Elle Hallal <daniellehallal@gmail.com>
2019-04-26 11:32:06 +0100

    GameFactory: amend setup to include validator, loader and filename)

--- spec/game_factory_spec.rb
+++ spec/game_factory_spec.rb
@@ -1,5 +1,7 @@
 require_relative './test_doubles/display_colour_double'
 require 'game_factory'
+require 'game_validator'
+require 'game_loader'
 require 'player_factory'
 require 'player_validator'
 require 'display'
@@ -9,17 +11,23 @@ def game_factory_setup
   display = Display.new(display_colour)
   player_validator = PlayerValidator.new(display)
   player_factory = PlayerFactory.new(player_validator, display)
-  GameFactory.new(player_factory)
+  game_validator = GameValidator.new(display)
+  game_loader = GameLoader.new
+  filename = './spec/test_data/game_factory_test.yml'
+  GameFactory.new(player_factory, game_validator, game_loader, filename)
 end
 
 RSpec.describe GameFactory do
-  describe 'Creating players:' do
+  describe 'Creating a new game:' do
+
+    game_factory = game_factory_setup
+    $stdout = StringIO.new
+
     it 'creates a human vs human game' do
       allow($stdin).to receive(:gets).and_return('H', 'H', '1')
-      squares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-      game_factory = game_factory_setup
-
-      game = game_factory.create_game(squares)
+      
+      game = game_factory.new_game([1, 2, 3, 4, 5, 6, 7, 8, 9])
+      allow(game.current_player).to receive(:sleep)
 
       expect(game.current_player).to be_an_instance_of(HumanPlayer)
       game.play_move
@@ -28,10 +36,9 @@ RSpec.describe GameFactory do
 
     it 'creates a computer vs computer game' do
       allow($stdin).to receive(:gets).and_return('C', 'C')
-      squares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-      game_factory = game_factory_setup
 
-      game = game_factory.create_game(squares)
+      game = game_factory.new_game([1, 2, 3, 4, 5, 6, 7, 8, 9])
+      allow(game.current_player).to receive(:sleep)
 
       expect(game.current_player).to be_an_instance_of(ComputerPlayer)
       game.play_move
@@ -40,10 +47,9 @@ RSpec.describe GameFactory do
 
     it 'creates a computer vs human game' do
       allow($stdin).to receive(:gets).and_return('C', 'H')
-      squares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-      game_factory = game_factory_setup
 
-      game = game_factory.create_game(squares)
+      game = game_factory.new_game([1, 2, 3, 4, 5, 6, 7, 8, 9])
+      allow(game.current_player).to receive(:sleep)
 
       expect(game.current_player).to be_an_instance_of(ComputerPlayer)
       game.play_move
