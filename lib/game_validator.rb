4d1bb39e3b46e03a249ff864d94d56782964033c
Elle Hallal <daniellehallal@gmail.com>
2019-04-26 12:00:17 +0100

    GameFactory: amend initialize method to accept instances of game validator, game loader and a file name. Add existing_game to return an instance from a game from a file. Add create game to manage if new or existing game should be returned

--- lib/game_validator.rb
+++ lib/game_validator.rb
@@ -5,6 +5,17 @@ class GameValidator
     @display = display
   end
 
+  def validate_game_type_input
+    print "Please enter 'new' to start a new game, or 'existing' to load an existing game:\n"
+    selection = $stdin.gets.chomp.downcase
+
+    until %w[new existing].include?(selection)
+      print "Invalid game type. Please enter 'new' or 'existing': "
+      selection = $stdin.gets.chomp.downcase
+    end
+    selection
+  end
+
   def existing_game_selection(filename)
     game_name = prompt_for_name
     until game_name_exists?(filename, game_name)
@@ -34,4 +45,6 @@ class GameValidator
     @display.invalid_game_message
     $stdin.gets.chomp
   end
+
+  
 end
--- spec/game_factory_spec.rb
+++ spec/game_factory_spec.rb
@@ -1,10 +1,12 @@
 require_relative './test_doubles/display_colour_double'
+require_relative './test_doubles/fake_class_double'
 require 'game_factory'
 require 'game_validator'
 require 'game_loader'
 require 'player_factory'
 require 'player_validator'
 require 'display'
+require 'yaml'
 
 def game_factory_setup
   display_colour = DisplayColourDouble.new
@@ -17,6 +19,13 @@ def game_factory_setup
   GameFactory.new(player_factory, game_validator, game_loader, filename)
 end
 
+def existing_game_setup
+  filename = './spec/test_data/game_factory_test.yml'
+  game_obj = FakeClassDouble.new(1, 2, 3)
+  game_details = { 'Great game' => game_obj }
+  File.open(filename, 'w') { |file| file.write game_details.to_yaml }
+end
+
 RSpec.describe GameFactory do
   describe 'Creating a new game:' do
 
@@ -24,9 +33,9 @@ RSpec.describe GameFactory do
     $stdout = StringIO.new
 
     it 'creates a human vs human game' do
-      allow($stdin).to receive(:gets).and_return('H', 'H', '1')
-      
-      game = game_factory.new_game([1, 2, 3, 4, 5, 6, 7, 8, 9])
+      allow($stdin).to receive(:gets).and_return('new','H', 'H', '1')
+
+      game = game_factory.create_game
       allow(game.current_player).to receive(:sleep)
 
       expect(game.current_player).to be_an_instance_of(HumanPlayer)
@@ -35,9 +44,9 @@ RSpec.describe GameFactory do
     end
 
     it 'creates a computer vs computer game' do
-      allow($stdin).to receive(:gets).and_return('C', 'C')
+      allow($stdin).to receive(:gets).and_return('new','C', 'C')
 
-      game = game_factory.new_game([1, 2, 3, 4, 5, 6, 7, 8, 9])
+      game = game_factory.create_game
       allow(game.current_player).to receive(:sleep)
 
       expect(game.current_player).to be_an_instance_of(ComputerPlayer)
@@ -46,14 +55,26 @@ RSpec.describe GameFactory do
     end
 
     it 'creates a computer vs human game' do
-      allow($stdin).to receive(:gets).and_return('C', 'H')
+      allow($stdin).to receive(:gets).and_return('new','C', 'H')
 
-      game = game_factory.new_game([1, 2, 3, 4, 5, 6, 7, 8, 9])
+      game = game_factory.create_game
       allow(game.current_player).to receive(:sleep)
 
       expect(game.current_player).to be_an_instance_of(ComputerPlayer)
       game.play_move
       expect(game.current_player).to be_an_instance_of(HumanPlayer)
+    end
+  end
+
+  describe 'Retrieving an existing game from a file:' do
+    it 'returns an existing instance of a class from the specified file' do
+      game_factory = game_factory_setup
+      existing_game_setup
+      allow($stdin).to receive(:gets).and_return('existing', 'Great game')
+
+      game = game_factory.create_game
+
+      expect(game).to be_an_instance_of(FakeClassDouble)
     end
   end
 end
