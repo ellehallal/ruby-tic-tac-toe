0c18a06199751075d459fdf34684e3bebb7d5bf3
Elle H <itsellej@gmail.com>
2019-04-28 11:26:30 +0100

    HumanPlayer: extract move validation to handle_move. Add save_or_exit to return input if it is 'save' or 'exit'

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,12 +1,28 @@
 require_relative 'player.rb'
 
 class HumanPlayer < Player
-  def choose_move(board, *args)
+  def choose_move(board, *_args)
     @display.display_board(board.squares)
     @display.show_current_player(mark, name)
     @display.ask_for_move
 
-    move = $stdin.gets.chomp.to_i
+    move = $stdin.gets.chomp
+
+    if save_or_exit?(move)
+      move
+    else
+      handle_move(board, move)
+    end
+  end
+
+  private
+
+  def save_or_exit?(move)
+    %w[exit save].include?(move)
+  end
+
+  def handle_move(board, move)
+    move = move.to_i
     until board.move_valid?(move)
       @display.invalid_move_message
       @display.ask_for_move
--- spec/human_player_spec.rb
+++ spec/human_player_spec.rb
@@ -35,4 +35,26 @@ RSpec.describe HumanPlayer do
         .to include('You have entered an invalid move. Please try again:')
     end
   end
+
+  describe 'Selecting save or exit' do
+    it 'returns "save" if entered by user' do
+      allow($stdin).to receive(:gets).and_return('save')
+      human_player = human_player_setup
+      board = Board.new([1, 'x', 3])
+
+      move = human_player.choose_move(board)
+
+      expect(move).to eq('save')
+    end
+
+    it 'returns "exit" if entered by user' do
+      allow($stdin).to receive(:gets).and_return('exit')
+      human_player = human_player_setup
+      board = Board.new([1, 'x', 3])
+
+      move = human_player.choose_move(board)
+
+      expect(move).to eq('exit')
+    end
+  end
 end
