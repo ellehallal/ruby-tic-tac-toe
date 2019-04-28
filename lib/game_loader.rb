1934557478f1e2ff47703a796cc180875685e1d2
Elle H <itsellej@gmail.com>
2019-04-28 14:52:26 +0100

    GameSaver and GameLoader: amend retrieve_file_contents to check for file.nil?

--- lib/game_loader.rb
+++ lib/game_loader.rb
@@ -1,5 +1,4 @@
 class GameLoader
-
   def load(filename, name)
     file = retrieve_file_contents(filename)
     file[name]
@@ -9,6 +8,6 @@ class GameLoader
 
   def retrieve_file_contents(filename)
     file = YAML.load_file(filename)
-    file == false ? {} : file
+    file == false || file.nil? ? {} : file
   end
 end
--- lib/game_saver.rb
+++ lib/game_saver.rb
@@ -1,17 +1,16 @@
 require 'yaml'
 
 class GameSaver
-
   def save(filename, name, object)
     game_storage = retrieve_file_contents(filename)
     game_storage[name] = object
-    File.open(filename, "w") { |file| file.write game_storage.to_yaml }
+    File.open(filename, 'w') { |file| file.write game_storage.to_yaml }
   end
 
   private
 
   def retrieve_file_contents(filename)
     file = YAML.load_file(filename)
-    file == false ? {} : file
+    file == false || file.nil? ? {} : file
   end
 end
